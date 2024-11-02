69a22d14e95392bf3b760e5baa9f6d22a6789f7b
saketh reddy <sakethreddy2510@gmail.com>
2024-11-01 22:59:33 -0400

    set view and view engines

--- src/app.js
+++ src/app.js
@@ -5,10 +5,23 @@ const path = require("path");
 const app = express();
 const getWeatherData = require("../utils/weatherData");
 
+const publicpath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicpath));
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index", {
+    title: "Weather App",
+  });
 });
 
 app.get("/weather", (req, res) => {
@@ -25,6 +38,9 @@ app.get("/weather", (req, res) => {
     }
   }
   getWeather();
+});
+app.get("*", (req, res) => {
+  res.send("404 Not Found");
 });
 
 app.listen(port, () => {
